b4b627891397717ced24f48145601e39eeb5c697
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-17 03:47:30 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -51,22 +51,22 @@ def naming_system
      sub_category: {
        label: "Phylum",
        sub_category: {
-         label: "Class"
+         label: "Class",
           sub_category: PORTION_2
        }
      }
    }
 
    PORTION_2 = {
-     label: "Order"
-      sub_category: PORTION_3
+     label: "Order",
+     sub_category: PORTION_3
    }
 
    PORTION_3 = {
      label: "Family",
      sub_category: {
        label: "Genus",
-        sub_category: PORTION_4
+       sub_category: PORTION_4
      }
    }
 
